e5ff1580f4537cb7b4cb4d095dd85f8147e61d0d
Robin Neatherway <robin.neatherway@gmail.com>
2015-06-11 22:19:43 +0100

    Add first pass of newfile command

---
+++ src/Projekt/FSharpProject.fs
@@ -0,0 +1,27 @@
+﻿namespace Projekt
+
+open System.Xml.Linq
+open XmlLinqHelpers
+open System
+
+type FSharpProject(path: string) =
+  let project = XDocument.Load(path)
+
+  let pathRelativeToPath (source: string) (target: string) =
+    let source =
+      if source.[source.Length - 1] = IO.Path.DirectorySeparatorChar ||
+         source.[source.Length - 1] = IO.Path.AltDirectorySeparatorChar
+      then
+        source + (string IO.Path.DirectorySeparatorChar)
+      else
+        source
+
+    (new Uri(source)).MakeRelativeUri(new Uri(target)).ToString()
+
+  member x.AddFile(file) =
+    let c = Seq.head (project.Descendants(xn (msbuildNamespace + "Compile")))
+    let e = xe (msbuildNamespace + "Compile") (xa "Include" (pathRelativeToPath path file))
+    c.AddBeforeSelf(e)
+
+  member x.Flush() =
+    project.Save(path)
